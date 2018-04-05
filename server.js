f4c37df1e96363a53026e431cbd08136a9a0c2dc
luckysher <luckyshera95@gmail.com>
2018-04-05 11:34:43 +0530

    updated server js file and added method for not found request

--- server.js
+++ server.js
@@ -2,6 +2,14 @@
 
 
 
+
+app.use(function(req, res, next){
+        res.statusCode = 404;
+        res.setHeader('content-type', 'text/html');
+
+    });
+
+
 app.listen(nserver.config.port, nserver.config.hostname, () => {
     console.log(`Server running at: http://${nserver.config.hostname}:${nserver.config.port}/`);
 });
