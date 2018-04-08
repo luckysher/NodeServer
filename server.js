c463e1cede07b7ca8bb0bdf1a3e0bb7e14e42b79
luckysher <luckyshera95@gmail.com>
2018-04-08 15:36:19 +0530

    updated not found url

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 const http =  require('http');
 const express =  require('express');
-const nserver = require('./config');
 
 
 var app = express();
@@ -28,8 +27,15 @@ function home(req, res){
 app.request(function(req, res, next){
         res.statusCode = 404;
         res.setHeader('content-type', 'text/html');
-        res.end('<h1 style="color:Red;">' + res.statusCode + " " + http.STATUS_CODES[res.statusCode] + " " + req.url + '</h1>');
-        info(req, res);
+        var response = '<p style="color:Red;">' + res.statusCode + " " + http.STATUS_CODES[res.statusCode] + " " + req.url + '</p><br/> Available urls: <br/> ';
+
+            var urls = nserver.urls;
+            var availUrls = '';
+            for(var url in urls){
+               availUrls = '<h3 style="margin-left:100px;color:blue;">' + availUrls + ` ${url} ` + ' : ' + urls[url] + '<br/></h3>';
+            }
+            res.end(response + availUrls);
+            info(req, res);
     });
 
 
