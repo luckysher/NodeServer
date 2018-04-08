3bd09020e33428a606903ce4c4b42780a246fb52
luckysher <luckyshera95@gmail.com>
2018-04-08 14:30:15 +0530

    added method for logging info

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 
 const http =  require('http');
 const express =  require('express');
+const nserver = require('./config');
 
+
+function info(req, res){
+    console.log(req.method + " " + res.statusCode + " " + http.STATUS_CODES[res.statusCode] + " " + req.url);
+}
 
 app.use(function(req, res, next){
         res.statusCode = 404;
